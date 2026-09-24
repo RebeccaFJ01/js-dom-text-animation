cf0c9c1666e4caf807fbbb4a423ef86d50431236
RebeccaFJ01 <rebeccaj.fyhn@gmail.com>
2026-09-24 12:27:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 // Husk fra dag 2: skriv "use strict" herunder
 
 
@@ -6,15 +7,24 @@ const getWordElem = document.getElementById("word");
 
 // Skriv selv: hent knappen "zoomBtn" på samme måde, ved hjælp af dens id. Variablen skal hedde getZoomBtn
 
+const getZoomBtn = document.getElementById("zoomBtn")
+
 
 // Eksempel: vi lytter efter klik på knappen og kører en anonym function, når der klikkes - ligesom i billedskift-opgaven
 getZoomBtn.addEventListener("click", function() {
 
+
+
+    
     // Skriv if/else-strukturen selv herinde, ligesom i billedskift-opgaven.
     //
     // Nyt i dag: getWordElem.style.fontSize ændrer en CSS-egenskab (fontSize) direkte via JavaScript,
     // på samme måde som getImage.src ændrede et billede i billedskift-opgaven.
     //
+    If (this.textContent = "Zoom ind") {
+        getWordElem.style.fontSize = 
+    }
+
     // Hvis this.textContent er "Zoom ind", så:
     //   - sæt getWordElem.style.fontSize til "6rem"
     //   - sæt this.textContent til "Zoom ud"
